2ea85347c9174e3c2e38c95a4614fcf7b5cb04e7
xuji <xuji@myaidoctor.com>
2017-12-12 14:00:31 +0800

    去掉了请求json，使用静态数据

--- src/views/dataShow/phoneCounts.vue
+++ src/views/dataShow/phoneCounts.vue
@@ -50,94 +50,96 @@
 		mounted() {
 			this._myChart()
 			this._recentYears()
-			this.searchTime()
+			// this.searchTime()
 		},
 		methods: {
 			_myChart() {
 				this._recentYears()
 
-				// let _myChart = echarts.init(document.getElementById('myChart'));
-				// let option = {
-				//     title: {
-				//         text: '随访系统呼叫任务完成情况(日)',
-				//         x: 'center',
-				//         subtext: '数据来自虚拟医生',
+				let _myChart = echarts.init(document.getElementById('myChart'));
+				let option = {
+				    title: {
+				        text: '随访系统呼叫任务完成情况(日)',
+				        x: 'center',
+				        subtext: '数据来自虚拟医生',
 
-				//     },
-				//     backgroundColor: '#e7e7e7',
+				    },
+				    backgroundColor: '#e7e7e7',
 				    
-				//     tooltip: {
-				//         trigger: 'axis',
-				//         axisPointer: {
-				//             type: 'shadow'
-				//         }
-				//     },
-				//     legend: {
-				//         data: ['实际打完的', '实际生成的(不包括昨日延时,或者未打完的)'],
-				//         align: 'left',
-				//         x: 'right',
-				//         y: '25px'
-				//     },
-				//     grid: {
-				//         left: '3%',
-				//         right: '4%',
+				    tooltip: {
+				        trigger: 'axis',
+				        axisPointer: {
+				            type: 'shadow'
+				        }
+				    },
+				    legend: {
+				        data: ['实际打完的', '实际生成的(不包括昨日延时,或者未打完的)'],
+				        align: 'left',
+				        x: 'right',
+				        y: '25px'
+				    },
+				    grid: {
+				        left: '3%',
+				        right: '4%',
 
 
-				//         bottom: '3%',
-				//         containLabel: true
-				//     },
-				//     yAxis: [
-				// 	    {
-				// 	        type: 'value',
-				// 	        boundaryGap: [0, 0.01]
-				// 	    }
-				//     ],
-				//     xAxis: [
-				// 	    {
-				// 	        type: 'category',
-				// 	        data: [
-				// 	        '2017-10-1','2017-10-2','2017-10-3','2017-10-4','2017-10-5','2017-10-6',
-				// 	        '2017-10-7','2017-10-8','2017-10-9','2017-10-10','2017-10-11','2017-10-12',
-				// 	        '2017-10-13','2017-10-14','2017-10-15','2017-10-16','2017-10-17','2017-10-18',
-				// 	        '2017-10-19','2017-10-20','2017-10-21','2017-10-22','2017-10-23','2017-10-24',
-				// 	        '2017-10-25','2017-10-26','2017-10-27','2017-10-28','2017-10-29','2017-10-30','2017-10-31'
-				// 	        ],
-				// 	        axisLabel: {
-				// 	            interval: 0,
-				// 	            rotate: 30
-				// 	        },
-				// 	    },{
+				        bottom: '3%',
+				        containLabel: true
+				    },
+				    yAxis: [
+					    {
+					        type: 'value',
+					        boundaryGap: [0, 0.01]
+					    }
+				    ],
+				    xAxis: [
+					    {
+					        type: 'category',
+					        data: [
+					        '2017-10-1','2017-10-2','2017-10-3','2017-10-4','2017-10-5','2017-10-6',
+					        '2017-10-7','2017-10-8','2017-10-9','2017-10-10','2017-10-11','2017-10-12',
+					        '2017-10-13','2017-10-14','2017-10-15','2017-10-16','2017-10-17','2017-10-18',
+					        '2017-10-19','2017-10-20','2017-10-21','2017-10-22','2017-10-23','2017-10-24',
+					        '2017-10-25','2017-10-26','2017-10-27','2017-10-28','2017-10-29','2017-10-30','2017-10-31'
+					        ],
+					        axisLabel: {
+					            interval: 0,
+					            rotate: 30
+					        },
+					    },{
 
-				// 	    }
-				//     ],
-				//     series: [
-				//         {
-				//             name: '实际生成的(不包括昨日延时,或者未打完的)',
-				//             type: 'bar',
-				//             data: [
-				//             18203, 23489, 29034, 104970, 131744, 630230,
-				//             18203, 23489, 29034, 104970, 131744, 630230,
-				//             18203, 23489, 29034, 104970, 131744, 630230,
-				//             18203, 23489, 29034, 104970, 131744, 630230, 
-				//             18203, 23489, 29034, 104970, 131744, 630230,
-				//             ]
-				//         },
-				//         {
-				//             name: '实际打完的',
-				//             type: 'bar',
-				//             data: [
-				//             19325, 23438, 31000, 121594, 134141, 681807,
-				//             19325, 23438, 31000, 121594, 134141, 681807,
-				//             19325, 23438, 31000, 121594, 134141, 681807, 
-				//             19325, 23438, 31000, 121594, 134141, 681807,
-				//             19325, 23438, 31000, 121594, 134141, 681807,
-				//             ]
-				//         }
-				//     ]
-				// };
+					    }
+				    ],
+				    series: [
+				        {
+				            name: '实际生成的(不包括昨日延时,或者未打完的)',
+				            type: 'bar',
+				            data: [
+				            18203, 23489, 29034, 104970, 131744, 630230,
+				            18203, 23489, 29034, 104970, 131744, 630230,
+				            18203, 23489, 29034, 104970, 131744, 630230,
+				            18203, 23489, 29034, 104970, 131744, 630230, 
+				            18203, 23489, 29034, 104970, 131744, 630230,
+				            ]
+				        },
+				        {
+				            name: '实际打完的',
+				            type: 'bar',
+				            data: [
+				            19325, 23438, 31000, 121594, 134141, 681807,
+				            19325, 23438, 31000, 121594, 134141, 681807,
+				            19325, 23438, 31000, 121594, 134141, 681807, 
+				            19325, 23438, 31000, 121594, 134141, 681807,
+				            19325, 23438, 31000, 121594, 134141, 681807,
+				            ]
+				        }
+				    ]
+				};
 
-				// _myChart.setOption(option)
+				_myChart.setOption(option)
 			},
+
+
 			_recentYears() {
 				let today = new Date();//获得当前日期
     			let year = today.getFullYear();//获得年份
@@ -169,91 +171,91 @@
 
 			},
 
-			searchTime() {
-				// if(this.selectYear == '') {
-				// 	alert('请选择年份')
-				// }
-				//  if(this.selectMonth == '') {
-				// 	alert('请选择月份')
-				// }
-				//if(this.selectYear != '' && this.selectMonth != ''){
-			      	axios.get('http://localhost:8888/static/phoneCount/phoneCount.json', {
-			      		year: '2017',
-			      		month: '10'
-			      	})
-			      	.then(function(response) {
-			      		const data = eval('(' + response.data +  ')');
-			      		console.log(data.detailTime)
-			      		let _myChart = echarts.init(document.getElementById('myChart'));
-						let option = {
-						    title: {
-						        text: '随访系统呼叫任务完成情况(日)',
-						        x: 'center',
-						        subtext: '数据来自虚拟医生',
+			// searchTime() {
+			// 	// if(this.selectYear == '') {
+			// 	// 	alert('请选择年份')
+			// 	// }
+			// 	//  if(this.selectMonth == '') {
+			// 	// 	alert('请选择月份')
+			// 	// }
+			// 	//if(this.selectYear != '' && this.selectMonth != ''){
+			//       	axios.get('http://localhost:8888/static/phoneCount/phoneCount.json', {
+			//       		year: '2017',
+			//       		month: '10'
+			//       	})
+			//       	.then(function(response) {
+			//       		const data = eval('(' + response.data +  ')');
+			//       		console.log(data.detailTime)
+			//       		let _myChart = echarts.init(document.getElementById('myChart'));
+			// 			let option = {
+			// 			    title: {
+			// 			        text: '随访系统呼叫任务完成情况(日)',
+			// 			        x: 'center',
+			// 			        subtext: '数据来自虚拟医生',
 
-						    },
-						    backgroundColor: '#e7e7e7',
+			// 			    },
+			// 			    backgroundColor: '#e7e7e7',
 						    
-						    tooltip: {
-						        trigger: 'axis',
-						        axisPointer: {
-						            type: 'shadow'
-						        }
-						    },
-						    legend: {
-						        data: ['实际打完的', '实际生成的(不包括昨日延时,或者未打完的)'],
-						        align: 'left',
-						        x: 'right',
-						        y: '25px'
-						    },
-						    grid: {
-						        left: '3%',
-						        right: '4%',
+			// 			    tooltip: {
+			// 			        trigger: 'axis',
+			// 			        axisPointer: {
+			// 			            type: 'shadow'
+			// 			        }
+			// 			    },
+			// 			    legend: {
+			// 			        data: ['实际打完的', '实际生成的(不包括昨日延时,或者未打完的)'],
+			// 			        align: 'left',
+			// 			        x: 'right',
+			// 			        y: '25px'
+			// 			    },
+			// 			    grid: {
+			// 			        left: '3%',
+			// 			        right: '4%',
 
 
-						        bottom: '3%',
-						        containLabel: true
-						    },
-						    yAxis: [
-							    {
-							        type: 'value',
-							        boundaryGap: [0, 0.01]
-							    }
-						    ],
-						    xAxis: [
-							    {
-							        type: 'category',
-							        data: data.detailTime,
-							        axisLabel: {
-							            interval: 0,
-							            rotate: 30
-							        },
-							    },{
+			// 			        bottom: '3%',
+			// 			        containLabel: true
+			// 			    },
+			// 			    yAxis: [
+			// 				    {
+			// 				        type: 'value',
+			// 				        boundaryGap: [0, 0.01]
+			// 				    }
+			// 			    ],
+			// 			    xAxis: [
+			// 				    {
+			// 				        type: 'category',
+			// 				        data: data.detailTime,
+			// 				        axisLabel: {
+			// 				            interval: 0,
+			// 				            rotate: 30
+			// 				        },
+			// 				    },{
 
-							    }
-						    ],
-						    series: [
-						        {
-						            name: '实际生成的(不包括昨日延时,或者未打完的)',
-						            type: 'bar',
-						            data: data.factCounts
-						        },
-						        {
-						            name: '实际打完的',
-						            type: 'bar',
-						            data: data.factCall
-						        }
-						    ]
-						};
+			// 				    }
+			// 			    ],
+			// 			    series: [
+			// 			        {
+			// 			            name: '实际生成的(不包括昨日延时,或者未打完的)',
+			// 			            type: 'bar',
+			// 			            data: data.factCounts
+			// 			        },
+			// 			        {
+			// 			            name: '实际打完的',
+			// 			            type: 'bar',
+			// 			            data: data.factCall
+			// 			        }
+			// 			    ]
+			// 			};
 
-						_myChart.setOption(option)
-			      	})
-			      	.catch(function(error) {
-			      		console.log(error)
-			      	})
-				//}
+			// 			_myChart.setOption(option)
+			//       	})
+			//       	.catch(function(error) {
+			//       		console.log(error)
+			//       	})
+			// 	//}
 
-			},
+			// },
 			handlechangeyear(value) {
 				this.selectYear = value
 			},

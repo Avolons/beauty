6ac340ff5193ead0b977b0e6a1499ac22838edf6
xuji <xuji@myaidoctor.com>
2018-01-16 16:38:13 +0800

    增加就诊档案

--- src/views/followBusiness/followPlan.vue
+++ src/views/followBusiness/followPlan.vue
@@ -46,6 +46,176 @@
                 </FormItem>
             </Form>
         </Modal>
+        <!-- 详情模态框 -->
+        <Modal v-model="patientDetail" title="患者信息" class-name="patientInfo" :styles="{top: '36px'}" width="1000">
+          <Row class="infoRow">
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">姓名</div>
+                  <div class="info2 bdx1">{{currentData.brxm}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">性别</div>
+                  <div class="info2">{{currentData.brxb}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">电话</div>
+                  <div class="info2 bdx1">{{currentData.jtdh}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">地址</div>
+                  <div class="info2">{{currentData.xzzQtdz}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">年龄</div>
+                  <div class="info2 bdx1">{{currentData.age}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">民族</div>
+                  <div class="info2">{{currentData.mz}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">出生年月</div>
+                  <div class="info2 bdx1">{{currentData.csny}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">身份证号</div>
+                  <div class="info2">{{currentData.sfzh}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">紧急联系人</div>
+                  <div class="info2 bdx1">{{currentData.lxrm}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">关系</div>
+                  <div class="info2">{{currentData.lxgx}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info">
+                <div class="info-row">
+                  <div class="info1 bb1">联系地址</div>
+                  <div class="info2 bdx1">{{currentData.lxdz}}</div>
+                </div>
+                <div class="info-row">
+                  <div class="info1">联系电话</div>
+                  <div class="info2">{{currentData.lxdh}}</div>
+                </div>
+              </div>
+            </Col>
+            <Col span="12" class="infoCol12 mb12">
+              <div class="info" style="height: 32px;">
+                <div class="info-row">
+                  <div class="info1 bdx1">单位</div>
+                  <div class="info2 bdx1">{{currentData.dwmc}}</div>
+                </div>
+              </div>
+            </Col>
+            <!-- 门诊 -->
+            <Col span="24" class="infoCol24" v-if="mjzData.length" v-for="item,index in mjzData" :key="index">
+              <Row class="infoRow2">
+                <Col span="4" class="sfCol4">
+                <div class="counts">
+                  <p class="suifang">门诊</p>
+                </div>
+                </Col>
+                <Col span="20" class="sfCol20">
+                <h3 class="sfName">{{currentData.brxm}}</h3>            
+                <Row>
+                  <Col span="12">
+                    <span>就诊卡号:</span><span>{{currentData.jzkh}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>患者性质:</span><span>{{currentData.brxz}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>就诊时间:</span><span>{{item.jzrq}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>是否初诊:</span><span>44</span>
+                  </Col>
+                  <Col span="12">
+                    <span>就诊科室:</span><span>{{item.ksmc}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>就诊医生:</span><span>{{item.ysxm}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>主诉:</span><span>{{item.zs}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>诊断:</span><span>{{item.zdmc}}</span>
+                  </Col>
+                </Row>
+                </Col>
+              </Row>
+            </Col>
+            <!-- 住院 -->
+            <Col span="24" class="infoCol24" v-if="zyData.length" v-for="item1,index1 in zyData" :key="index1">
+              <Row class="infoRow2">
+
+                <Col span="4" class="sfCol4">
+                <div class="counts">
+                  <p class="suifang">住院</p>
+                </div>
+                </Col>
+                <Col span="20" class="sfCol20">
+                <h3 class="sfName">{{currentData.brxm}}</h3>            
+                <Row>
+                  <Col span="12">
+                    <span>住院号:</span><span>{{item1.zyhm}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>患者性质:</span><span>{{currentData.brxz}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>入院时间:</span><span>{{item1.admissiontime}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>出院时间:</span><span>{{item1.leavetime}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>入院科别:</span><span>{{item1.departname}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>主治医生:</span><span>{{item1.doctorname}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>入院诊断:</span><span>{{item1.admissiondiagnose}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>出院诊断:</span><span>{{item1.leavediagnose}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>入院情况:</span><span>{{item1.admissiondescription}}</span>
+                  </Col>
+                  <Col span="12">
+                    <span>出院情况:</span><span>{{item1.leavedescription}}</span>
+                  </Col>
+                </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Modal>
     </Row>
 </template>
 
@@ -53,126 +223,161 @@
 import { API } from '@/services/index.js';
 export default {
     data() {
-        return {
-            //搜索选项
-            searchParams: {
-                brxm: '',//患者姓名
-                schemeName: '',//随访方案
-                status: '',//审核状态
-                pager: 1,//
-                limit:10,//每页条数
+      return {
+        //搜索选项
+        searchParams: {
+            brxm: '',//患者姓名
+            schemeName: '',//随访方案
+            status: '',//审核状态
+            pager: 1,//
+            limit:10,//每页条数
+        },
+        //列表配置
+        config: [
+            {
+                title: '患者姓名',
+                key: 'brxm',
+                align: 'center',
+                render: (h, params)=>{
+                  return h('div', [
+                    h('span', {
+                      props: {
+                          type: 'primary',
+                          size: 'small'
+                      },
+                      style: {
+                          borderBottom: '1px solid #5cadff',
+                          color: '#5cadff'
+                      },
+                      on: {
+                        click: () => {
+                          this.getInfo(params.row.hzxxId);
+                          this.patientDetail = true;
+                        }
+                      }
+                    }, params.row.brxm),
+                  ])
+                }
             },
-            //列表配置
-            config: [
-                {
-                    title: '患者姓名',
-                    key: 'brxm',
-                    align: 'center'
-                },
-                {
-                    title: '随访方案',
-                    key: 'schemeName',
-                    align: 'center'
-                },
-
-                {
-                    title: '审核状态',
-                    key: 'statusStr',
-                    align: 'center'
-                },
-                {
-                    title: '生成时间',
-                    key: 'dateAdd',
-                    align: 'center'
-                },
-                {
-                    title: '审核时间',
-                    key: 'dateVet',
-                    align: 'center'
-                },
-                {
-                    title: '操作',
-                    key: 'action',
-                    width: 250,
-                    align: 'center',
-                    /** 
-                     * 按钮状态
-                     * 随访中  停止随访  删除
-                     * 未开始  随访     删除
-                     * 已暂停  恢复随访  删除
-                     * 完成    删除
-                     */
-                    render: (h, params) => {
-                        return h('div', [
-                            h('Button', {
-                                props: {
-                                    type: 'primary',
-                                    size: 'small'
-                                },
-                                style: {
-                                    marginRight: '5px'
-                                },
-                                'class':{
-									menuHide:this.menuShow(this.AM.FollowBussiness.startPlan)
-								},
-                                on: {
-                                    click: () => {
-                                        this.startPlan(params.row.id);
-                                    }
-                                }
-                            }, '随访'),
-                            h('Button', {
-                                props: {
-                                    type: 'warning',
-                                    size: 'small'
-                                },
-                                style: {
-
-                                },
-                                'class':{
-									menuHide:this.menuShow(this.AM.FollowBussiness.delPlan)
-								},
-                                on: {
-                                    click: () => {
-                                        /** 
-                                         * 删除计划
-                                         */
-                                        this.deletPlan(params.row.id);
-                                    }
-                                }
-                            }, '删除')
-                        ]);
-                    }
-                }],
-            //列表数据
-            dataList: [],
-            statusList: [{
-                name:"全部",
-                id:""
-            },{
-                name:"待审核",
-                id:0
-            },{
-                name:"不通过",
-                id:1
-            },{
-                name:"审核通过",
-                id:2
-            },{
-                name:"已排期",
-                id:3
-            },{
-                name:"已取消",
-                id:4
-            },],//审核状态选项列表
-            id: -1,//当前被选中的数据id
-            totalPage: 100,//总页数
-            followShow: false,//编辑模态框
-            //随访数据
-            AIform: {
-                AIphone: '',
+            {
+                title: '随访方案',
+                key: 'schemeName',
+                align: 'center'
             },
-        }
+
+            {
+                title: '审核状态',
+                key: 'statusStr',
+                align: 'center'
+            },
+            {
+                title: '生成时间',
+                key: 'dateAdd',
+                align: 'center'
+            },
+            {
+                title: '审核时间',
+                key: 'dateVet',
+                align: 'center'
+            },
+            {
+                title: '操作',
+                key: 'action',
+                width: 250,
+                align: 'center',
+                /** 
+                 * 按钮状态
+                 * 随访中  停止随访  删除
+                 * 未开始  随访     删除
+                 * 已暂停  恢复随访  删除
+                 * 完成    删除
+                 */
+                render: (h, params) => {
+                    return h('div', [
+                        h('Button', {
+                            props: {
+                                type: 'primary',
+                                size: 'small'
+                            },
+                            style: {
+                                marginRight: '5px'
+                            },
+                            'class':{
+                							menuHide:this.menuShow(this.AM.FollowBussiness.startPlan)
+                						},
+                            on: {
+                                click: () => {
+                                    this.startPlan(params.row.id);
+                                }
+                            }
+                        }, '随访'),
+                        h('Button', {
+                            props: {
+                                type: 'warning',
+                                size: 'small'
+                            },
+                            style: {
+
+                            },
+                            'class':{
+                							menuHide:this.menuShow(this.AM.FollowBussiness.delPlan)
+                						},
+                            on: {
+                                click: () => {
+                                    /** 
+                                     * 删除计划
+                                     */
+                                    this.deletPlan(params.row.id);
+                                }
+                            }
+                        }, '删除')
+                    ]);
+                }
+            }],
+        //列表数据
+        dataList: [],
+        statusList: [
+          {
+            name:"全部",
+            id:""
+          },
+          {
+            name:"待审核",
+            id:0
+          },
+          {
+            name:"不通过",
+            id:1
+          },
+          {
+            name:"审核通过",
+            id:2
+          },
+          {
+            name:"已排期",
+            id:3
+          },
+          {
+            name:"已取消",
+            id:4
+          }
+        ],//审核状态选项列表
+        id: -1,//当前被选中的数据id
+        totalPage: 100,//总页数
+        followShow: false,//编辑模态框
+        //随访数据
+        AIform: {
+          AIphone: '',
+        },
+        //当前被点击患者，编辑和详情按钮触发时更换数据
+        currentData: {},
+        //门急诊信息
+        mjzData: [],
+        //住院信息
+        zyData: [],
+        //详情模态框
+        patientDetail: false,
+      }
     },
     methods: {
         /** 
@@ -247,14 +452,33 @@ export default {
          */
         stopPlan(id) {
             API.FollowBussiness.listPat(this.searchParams).then((res)=>{
-				this.dataList=res.data;
-				this.totalPage=res.total;
-			}).catch((err)=>{
+      				this.dataList=res.data;
+      				this.totalPage=res.total;
+      			}).catch((err)=>{
 
-			});
+      			});
             /* 暂停后在回调中刷新当前数据 */
             this.getData();
-        }
+        },
+      /** 
+       * 查看患者详情
+       */
+      getInfo(id) {
+        API.FollowBussiness.detailPat({
+          id: id
+        }).then((res) => {
+          if(res.data.mjzs.length) {
+            this.mjzData = res.data.mjzs
+          }
+          if(res.data.cyxjs.length) {
+            this.zyData = res.data.cyxjs
+          } 
+          this.currentData = res.data;
+        }).catch((err) => {
+          console.log(err)
+
+        });
+      },
     },
     mounted () {
         this.getData();
@@ -264,6 +488,7 @@ export default {
 
 <style lang="less">
 @import "../../styles/common.less";
+@import "../../styles/jzda.less";
 .inter-down {
     &_main {
         &_list {}
